e797a9f81a1fa7b0f9f568be267e3fc5447e67fc
Khánh Vương <khanhvuong8939@gmail.com>
2019-01-29 20:38:14 +0700

    complete products container

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,25 +3,16 @@ import Product from './Product';
 
 class Products extends Component {
     render() {
-        let { products } = this.props;
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
-                    {this.showProduct(products)}
+                    {this.props.children}
                 </div>
             </section>
         );
     }
-    showProduct = (products) => {
-        let result = [];
-        if (products.length > 0) {
-            products.map((product, index) => {
-                return result.push(<Product key={index} product={product} />)
-            })
-        }
-        return result;
-    }
+
 }
 
 export default Products;
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Products from './../components/Products';
+import Product from './../components/Product';
 
 import { connect } from 'react-redux';
 
@@ -7,10 +8,23 @@ class ProductsContainer extends Component {
     render() {
         let { products } = this.props;
         return (
-            <Products products={products} />
+            <Products>
+                {this.showProduct(products)}
+            </Products>
         );
     }
+    showProduct = (products) => {
+        let result = [];
+        if (products.length > 0) {
+            products.map((product, index) => {
+                return result.push(<Product key={index} product={product} />)
+            })
+        }
+        return result;
+    }
 }
+
+
 
 var mapStateToProps = state => {
     return { products: state.products }
