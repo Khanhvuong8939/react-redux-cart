c479319e44e4757070ec1247699c2caf3c8bd01a
vuong.tk <vuong.tk@gnt.com.vn>
2019-01-29 11:10:12 +0700

    add Css/js... Seperate Component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Products from './components/Products';
+import Cart from './components/Cart';
+import Footer from './components/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <main id="mainContainer">
+                    <div className="container">
+                        <Products />
+                        <h3>
+                            <span className="badge amber darken-2">Mua Hàng Thành Công !</span>
+                        </h3>
+                        <Cart />
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
